bc93528afa9ed0de6347e7ffd643d28379c630b2
Firstname Lastname <mayin1q84@gmail.com>
2016-06-16 00:24:13 +0900

    edit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :bnet, "p6cxxnqks8k3xrevexv8pwk27bs83cxg", "TeWVkS9Tg9zPmkkXxaKFChCZWukYWff3", scope: "wow.profile sc2.profile"
-  OmniAuth.config.full_host = "https://localhost:3000"
+  if Rails.env == 'development'
+    OmniAuth.config.full_host = "https://localhost:3000"
+  else
+    OmniAuth.config.full_host = "https://happyhouse.me"
+  end
+  
 
   enable :sessions
   enable :inline_templates
